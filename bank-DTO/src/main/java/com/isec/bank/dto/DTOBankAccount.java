8a0de7f7f51d9bb09652b44c949bfa4861e3206d
luisarcher <luismcjordao@hotmail.com>
2019-11-17 13:44:38 +0000

    account dto

--- bank-DTO/src/main/java/com/isec/bank/dto/DTOBankAccount.java
+++ bank-DTO/src/main/java/com/isec/bank/dto/DTOBankAccount.java
@@ -5,6 +5,8 @@
  */
 package com.isec.bank.dto;
 
+import java.util.Date;
+
 /**
  *
  * @author ljordao
@@ -13,5 +15,52 @@ public class DTOBankAccount {
     
     private static final long serialVersionUID = 1L;
 
+    private int idAccount;
+    private Double balance;
+    private Date created_at;
+    private int status;
+
+    public DTOBankAccount() {
+    }
+
+    public DTOBankAccount(int idAccount, Double balance, Date created_at, int status) {
+        this.idAccount = idAccount;
+        this.balance = balance;
+        this.created_at = created_at;
+        this.status = status;
+    }
+
+    public int getIdAccount() {
+        return idAccount;
+    }
+
+    public void setIdAccount(int idAccount) {
+        this.idAccount = idAccount;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
     
 }
